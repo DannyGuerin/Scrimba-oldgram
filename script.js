715607062e996bfa3e5f07be7ef239b7baf14bf5
Danny Guerin <danny.guerin01@gmail.com>
2024-11-15 12:27:19 +0000

    Project Complete
    All JS has now been added and is in line with the project requirements.

--- script.js
+++ script.js
@@ -27,3 +27,43 @@ const posts = [
       likes: 152
   }
 ]
+
+const mainWrap = document.querySelector(".main-wrap")
+
+posts.forEach((post, index) => {
+  mainWrap.innerHTML += `
+    <div class="post-container">
+            <div class="artist-avatar">
+                <img src="${post.avatar}" alt="avatar image of ${post.name}" class="avatar-image">
+                <div class="artist-text">
+                    <p class="bold-text">${post.name}</p>
+                    <p class="location">${post.location}</p>
+                </div>
+            </div>
+            <img src="${post.post}" alt="Oldagram post image" class="post-image">
+            <div class="interactions">
+                <img src="images/icon-heart.png" alt="icon-heart" class="icon like-btn" data-index="${index}">
+                <img src="images/icon-comment.png" alt="icon-comment" class="icon">
+                <img src="images/icon-dm.png" alt="icon-dm" class="icon">
+            </div>
+            <div class="text-wrap">
+                <p class="bold-text likes-count" data-index="${index}">${post.likes} likes</p>
+                <p class="post-desc"><span class="bold-text">${post.username}</span> ${post.comment}</p>
+            </div>
+        </div>
+  `
+})
+
+const likeButtons = document.querySelectorAll(".like-btn");
+
+likeButtons.forEach((button) => {
+  button.addEventListener("click", function () {
+    const postIndex = button.dataset.index;
+    posts[postIndex].likes++;
+
+    const likesText = document.querySelector(`.likes-count[data-index="${postIndex}"]`);
+    likesText.textContent = `${posts[postIndex].likes} likes`;
+  })
+})
+
+console.log("JS is connected");
